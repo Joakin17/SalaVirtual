349bb5f23594223be269d4b4e128adab630a2fe8
castillorobin <robin.castillo@catolica.edu.sv>
2023-02-18 11:25:41 -0600

    conbase

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="/imgs/logo.png"
+        <img src="imgs/logo.png"
              alt="AdminLTE Logo"
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">Biblioteca UNICAES</span>
